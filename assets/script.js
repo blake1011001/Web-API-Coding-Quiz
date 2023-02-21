f540f0e3e66cd6bb2907e6bc430afcc31e29f38e
Blake Sanders <blakesand00@gmail.com>
2023-02-21 18:13:08 -0500

    added questions and answers to js file

---
+++ assets/script.js
@@ -0,0 +1,49 @@
+var questions = [
+    {
+        question: "A useful tool for debugging and web development that helps print content to the console is called what?",
+        options: {
+            A: "boolean", 
+            B: "array", 
+            C: "console.log", 
+            D: "curly brackets",
+        }, 
+        correctAnswer: "console.log"    
+    },
+    {
+        question: "Boolean operators that can be used in JavaScript include which?",
+        choices: {
+            A: "'not' operator !",
+            B: "'and' operator &&",
+            C: "'or' operator ||",
+            D: "all of the above",
+        },
+        correctAnswer: "all of the above"
+    },
+    {
+        question: "Commonly used data types do not include which?",
+        options: {
+            A: "booleans",
+            B: "alerts",
+            C: "numbers",
+            D: "strings"
+        },
+        correctAnswer: "alerts"
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: {
+            A: "quotes",
+            B: "parentheses",
+            C: "commas",
+            D: "curly brackets",
+        },
+        correctAnswer: "quotes"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store which?"
+        options: {
+            
+        }
+    }
+
+];
